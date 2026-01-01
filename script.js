1ac30318df1ab354411d49b3e51bad4aa7fe6705
AliRaza <alirq.dev@gmail.com>
2026-01-01 14:57:02 -0500

    Responsive and Nav animation final push.

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
+function locomotiveAnimations() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
     smooth: true
+  });
+
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight
+      };
+    },
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed"
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+}
+locomotiveAnimations();
+
+function navAnimation(){
+    // ✅ GSAP animation AFTER locomotive setup
+gsap.to(".nav-part1 svg", {
+  yPercent: -100,
+  scrollTrigger: {
+    trigger: ".page1",
+    scroller: ".main",
+    // markers: true,
+    start: "top top",
+    end: "top -5%",
+    scrub: true
+  }
 });
+
+gsap.to(".nav-part2 .links", {
+  yPercent: -100,
+  opacity:0,
+  scrollTrigger: {
+    trigger: ".page1",
+    scroller: ".main",
+    // markers: true,
+    start: "top top",
+    end: "top -5%",
+    scrub: 1
+  }
+});
+}
+
 
 
 
@@ -49,19 +107,16 @@ function loadingTitleAnim() {
     
 
 }
- gsap.from(".img-container", {
+function imgContainerLoadingAni() {
+    gsap.from(".img-container", {
         scale: 1.1,
         opacity:0,
         duration:0.8,
         delay: 1.3,
         ease:"power4.out",
     })
-
-
-// Calling the animated functions
-
-// imgHoverEffect();
-loadingTitleAnim();
+}
+function cursorAniProducts() {
 
 document.addEventListener("mousemove", (dets)=>{
     gsap.to(".cursor", {
@@ -86,3 +141,12 @@ a.forEach(function(elem){
         })
     })
 })
+}
+
+// Calling the animated functions
+navAnimation();
+// imgHoverEffect();
+loadingTitleAnim();
+cursorAniProducts();
+imgContainerLoadingAni();
+
